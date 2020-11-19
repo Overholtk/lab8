882acf541758b0c6de2cbed2ed71b66ccd42f2b5
overholtk <overholtkaya@gmail.com>
2020-11-18 20:53:32 -0800

    initial setup

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.listen(3000, () => {
+  console.log('server up on port 3000');
+});
